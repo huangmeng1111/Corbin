5a5331d08970da0c42acf7724706481cd9717a24
荒梦 <1350160375@qq.com>
2025-08-17 20:00:08 +0800

    Add files via upload

---
+++ src/main/java/com/super_awesome_baby/examplemod/ExampleMod.java
@@ -0,0 +1,10 @@
+package com.super_awesome_baby.examplemod;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod
+{
+    public static final String MODID = "examplemod";
+}
