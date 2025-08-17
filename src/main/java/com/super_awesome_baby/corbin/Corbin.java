6fe6c3d1a701bdffb337b9320a6fefa40fb634a1
荒梦 <1350160375@qq.com>
2025-08-17 20:11:42 +0800

    初次提交

---
+++ src/main/java/com/super_awesome_baby/corbin/Corbin.java
@@ -0,0 +1,10 @@
+package com.super_awesome_baby.corbin;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Corbin.MODID)
+public class Corbin
+{
+    public static final String MODID = "corbin";
+}
